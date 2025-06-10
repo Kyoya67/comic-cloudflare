df6dd83a94eb77193c71b0a98af4b599691b7abe
Kyoya67 <jackma8569@gmail.com>
2025-06-10 19:44:28 +0900

    feat: add r2 upload api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -18,28 +18,43 @@ app.get("/comics", async (c) => {
 app.post("/upload", async (c) => {
     const formData = await c.req.formData();
     const title = formData.get('title');
-    const file = formData.get('file') as File;
+    const fileData = formData.get('file');
+
+    if (!fileData) {
+        return c.json({ message: 'File not found' }, 400);
+    }
+
+    const file = fileData as File;
+    const fileName = `${Date.now()}-${file.name}`;
 
     try {
-        const db = drizzle(
-            (getCloudflareContext().env as any).DB as unknown as D1Database
-        );
+        const r2 = (getCloudflareContext().env as any).R2 as unknown as R2Bucket;
+        await r2.put(fileName, file);
+    } catch (r2Error) {
+        return c.json({ success: false, message: 'R2 not found', error: r2Error }, 500);
+    }
 
-        const result = await db
-            .select({ maxOrder: sql`MAX("order")` })
-            .from(comics);
-        const nextOrder = Number(result[0].maxOrder) + 1;
+    const db = drizzle(
+        (getCloudflareContext().env as any).DB as unknown as D1Database
+    );
 
+    const result = await db
+        .select({ maxOrder: sql`MAX("order")` })
+        .from(comics);
+
+    const nextOrder = Number(result[0].maxOrder) + 1;
+
+    try {
         await db.insert(comics).values({
             title: title as string,
             order: nextOrder,
-            imageUrl: file.name as string,
+            imageUrl: fileName,
             updatedAt: new Date().toISOString(),
         });
     } catch (error) {
-        return c.json({ message: 'Comic uploaded failed' }, 500);
+        return c.json({ success: false, message: 'Comic uploaded failed' }, 500);
     }
-    return c.json({ message: 'Comic uploaded successfully' });
+    return c.json({ success: true, message: 'Comic uploaded successfully' });
 });
 
 export const GET = handle(app);
