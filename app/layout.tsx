1b3ad761a41ce7f07ded2e11c54fa4ce96cbf141
Kyoya67 <jackma8569@gmail.com>
2025-06-11 12:58:37 +0900

    fix: revert 226d6b47de53c175c4bf2e30753859deff729c7b state

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
+import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "./globals.css";
 
 // Next.js 14で利用可能なフォントに変更
 const inter = Inter({
